e63bb38c87e1dca80dc9e787b9c8dcb51103b170
Your Name <you@example.com>
2022-05-11 17:25:55 -0500

    ok

---
+++ controlador.js
@@ -0,0 +1,174 @@
+let producto1={
+    nombre:"producto1",
+    precio:15000,
+    estado: true,
+    foto:"https://firebasestorage.googleapis.com/v0/b/clinicadelplay-cf1d6.appspot.com/o/12069774_1.jfif?alt=media&token=0aed271b-7961-4654-8e23-aa30bffeb7fd"
+}
+ 
+let producto2={
+    nombre:"producto2",
+    precio:75000,
+    estado: true,
+    foto:"https://firebasestorage.googleapis.com/v0/b/clinicadelplay-cf1d6.appspot.com/o/617JNobX2VL._SL1215_.jpg?alt=media&token=8ffedf9d-ffb7-447f-9ba0-b4cecfd6f258"
+}
+ 
+let producto3={
+    nombre:"producto3",
+    precio:150000,
+    estado: true,
+    foto:"https://firebasestorage.googleapis.com/v0/b/clinicadelplay-cf1d6.appspot.com/o/619FrDaqfAL._AC_SY355_.jpg?alt=media&token=79d05b17-5c57-42af-bcfe-69c5989ec422"
+}
+ 
+let producto4={
+    nombre:"producto4",
+    precio:150000,
+    estado: true,
+    foto:"https://firebasestorage.googleapis.com/v0/b/clinicadelplay-cf1d6.appspot.com/o/61Kda%2BeUmlL._SL1000_.jpg?alt=media&token=67359025-390d-46d9-bfe2-3f14ccca7a07    "
+}
+ 
+let producto5={
+    nombre:"producto5",
+    precio:45000,
+    estado: true,
+    foto:"https://firebasestorage.googleapis.com/v0/b/clinicadelplay-cf1d6.appspot.com/o/61Om-iwnAbL._AC_SY450_.jpg?alt=media&token=81c7f41c-a715-47c2-86ca-83a15db8c751"
+}
+ 
+let producto6={
+    nombre:"producto6",
+    precio: 75000,
+    estado: true,
+    foto:"https://firebasestorage.googleapis.com/v0/b/clinicadelplay-cf1d6.appspot.com/o/61zWIw7-S3S._SX342_.jpg?alt=media&token=c39c21fd-dd94-4a8c-9eea-440ae0575b1f"
+}
+ 
+let producto7={
+    nombre:"producto7",
+    precio:75000,
+    estado: true,
+    foto:"https://firebasestorage.googleapis.com/v0/b/clinicadelplay-cf1d6.appspot.com/o/81n2llGXyiL._SX342_.jpg?alt=media&token=6c60563a-9747-432b-b001-4166123315f4"
+}
+ 
+let producto8={
+    nombre:"producto8",
+    precio:180000,
+    estado: true,
+    foto:"https://firebasestorage.googleapis.com/v0/b/clinicadelplay-cf1d6.appspot.com/o/9440503_2.jfif?alt=media&token=2c6b7aea-1be8-4be1-a4c9-49cb2d44ff56"
+}
+ 
+let producto9={
+    nombre:"producto9",
+    precio:300000,
+    estado: true,
+    foto:"https://firebasestorage.googleapis.com/v0/b/clinicadelplay-cf1d6.appspot.com/o/Aud%C3%ADfonos-Sony-Ps4-Gold-Wireless-Heasdet-Ps4.jpg?alt=media&token=2d4a44ae-0f15-4c65-9c24-ee4f6fff08a5"
+}
+ 
+let producto10={
+    nombre:"producto10",
+    precio:15000,
+    estado: true,
+    foto:"https://firebasestorage.googleapis.com/v0/b/clinicadelplay-cf1d6.appspot.com/o/D_NQ_NP_742938-MCO40052446796_122019-O.webp?alt=media&token=af451703-4530-4179-b673-28d011c603b5"
+}
+ 
+let producto11={
+    nombre:"producto11",
+    precio:15000,
+    estado: true,
+    foto:"https://firebasestorage.googleapis.com/v0/b/clinicadelplay-cf1d6.appspot.com/o/D_NQ_NP_797194-MCO43412176402_092020-O.jpg?alt=media&token=e3655d92-f730-41b9-a610-dce877ccc4a2"
+}
+ 
+let producto12={
+    nombre:"producto12",
+    precio:190000,
+    estado: false,
+    foto:"https://firebasestorage.googleapis.com/v0/b/clinicadelplay-cf1d6.appspot.com/o/PS5-DualSense-MidnightBlack.jpg?alt=media&token=facdaf23-b00b-4ffa-a890-782f057025e7   "
+}
+ 
+let producto13={
+    nombre:"producto13",
+    precio:5000,
+    estado: true,
+    foto:"https://firebasestorage.googleapis.com/v0/b/clinicadelplay-cf1d6.appspot.com/o/bdedc49a1ea3bc7f7a643b519c5bedf8-product.jpg?alt=media&token=9a0e98f9-91fe-4ad6-88e1-0faf367f0e00"
+}
+ 
+let producto14={
+    nombre:"producto14",
+    precio:296000,
+    estado: true,
+    foto:"https://firebasestorage.googleapis.com/v0/b/clinicadelplay-cf1d6.appspot.com/o/g502-lightspeed-hero.png?alt=media&token=dae69214-511f-490f-bff0-6c020ef3996a"
+}
+ 
+let producto15={
+    nombre:"producto15",
+    precio:500000,
+    estado: false,
+    foto:"https://firebasestorage.googleapis.com/v0/b/clinicadelplay-cf1d6.appspot.com/o/tv.jpg?alt=media&token=fe2924bb-d1ba-4bbf-b4f9-13846245fd33"
+}
+let producto16={
+    nombre:"producto16",
+    precio:4300000,
+    estado: true,
+    foto:"https://firebasestorage.googleapis.com/v0/b/clinicadelplay-cf1d6.appspot.com/o/iphone-11-pro-max.jpg?alt=media&token=3e98a0af-d086-495c-afb8-6b60a2734587"
+}
+let producto17={
+    nombre:"producto17",
+    precio:5600000,
+    estado: true,
+    foto:"https://firebasestorage.googleapis.com/v0/b/clinicadelplay-cf1d6.appspot.com/o/iphone-12-pro-max.jfif?alt=media&token=209457d4-6051-4b94-a84a-8cd459d633c4"
+}
+let producto18={
+    nombre:"producto18",
+    precio:5200000,
+    estado: true,
+    foto:"https://firebasestorage.googleapis.com/v0/b/clinicadelplay-cf1d6.appspot.com/o/iphone-13-mini-blue.jfif?alt=media&token=cf4879f8-8ebc-4d3f-9b83-8efc64718273"
+}
+let producto19={
+    nombre:"producto19",
+    precio:6300000,
+    estado: true,
+    foto:"https://firebasestorage.googleapis.com/v0/b/clinicadelplay-cf1d6.appspot.com/o/iphone13pro.jpg?alt=media&token=14f77760-46d7-49a7-83e4-fa749715816e"
+}
+let producto20={
+    nombre:"producto20",
+    precio:3900000,
+    estado: true,
+    foto:"https://firebasestorage.googleapis.com/v0/b/clinicadelplay-cf1d6.appspot.com/o/iphone13.jfif?alt=media&token=b25e7cf5-413b-42fc-b7b8-7841b1d0c822"
+}
+ 
+//ARREGLO
+let productos=Array(
+    producto1,
+    producto2,
+    producto3,
+    producto4,
+    producto5,
+    producto6,
+    producto7,
+    producto8,
+    producto9,
+    producto10,
+    producto11,
+    producto12,
+    producto13,
+    producto14,
+    producto15,
+    producto16,
+    producto17,
+    producto18,
+    producto19,
+    producto20
+)
+ 
+let contenedor = document.getElementById("contenedor")
+productos.forEach(function(producto){
+
+    let foto = document.createElement("img")
+    foto.classList.add("w-100", "img-fluid")
+    foto.src = producto.foto
+    let columna = document.createElement("div")
+    columna.classList.add("col")
+    let tarjeta = document.createElement("div")
+    tarjeta.classList.add("card")
+    tarjeta.appendChild(foto)
+    columna.appendChild(tarjeta)
+    contenedor.appendChild(columna)
+    
+})
