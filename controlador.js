47eca36199168e25661c7df50a7f4ab7eff25306
juan <tipito4085@gmail.com>
2022-05-17 17:19:53 -0500

    New catalog

--- controlador.js
+++ controlador.js
@@ -1,136 +1,176 @@
 let producto1={
-    nombre:"producto1",
-    precio:15000,
+    nombre:"TECLADO MINITAKA RGB",
+    precio:247000,
     estado: true,
-    foto:"https://firebasestorage.googleapis.com/v0/b/clinicadelplay-cf1d6.appspot.com/o/12069774_1.jfif?alt=media&token=0aed271b-7961-4654-8e23-aa30bffeb7fd"
+    foto:"https://firebasestorage.googleapis.com/v0/b/tiendasufragio.appspot.com/o/teclado%20minitaka.webp?alt=media&token=e52433b9-1120-4a8a-9288-95f792e54bf2",
+    popularidad: "5/5",
+    pais: "USA"
 }
  
 let producto2={
-    nombre:"producto2",
-    precio:75000,
-    estado: true,
-    foto:"https://firebasestorage.googleapis.com/v0/b/clinicadelplay-cf1d6.appspot.com/o/617JNobX2VL._SL1215_.jpg?alt=media&token=8ffedf9d-ffb7-447f-9ba0-b4cecfd6f258"
+    nombre:"LG 29' IPS ULTRAWIDE",
+    precio:1000000,
+    estado: false,
+    foto:"https://firebasestorage.googleapis.com/v0/b/tiendasufragio.appspot.com/o/MONITOR%20lg.webp?alt=media&token=d3082a49-0895-4f12-bda8-d2f8a04a07f2",
+    popularidad: "4/5",
+    pais: "USA"
 }
  
 let producto3={
-    nombre:"producto3",
-    precio:150000,
+    nombre:"SILLA GAMER R2 PURPLE",
+    precio:1250000,
     estado: true,
-    foto:"https://firebasestorage.googleapis.com/v0/b/clinicadelplay-cf1d6.appspot.com/o/619FrDaqfAL._AC_SY355_.jpg?alt=media&token=79d05b17-5c57-42af-bcfe-69c5989ec422"
+    foto:"https://firebasestorage.googleapis.com/v0/b/tiendasufragio.appspot.com/o/silla%20morada.webp?alt=media&token=1b51caf1-9f33-4dfd-8fea-9d6b055ee863",
+    popularidad: "3/5",
+    pais: "USA"
 }
  
 let producto4={
-    nombre:"producto4",
-    precio:150000,
+    nombre:"SILLA GAMER R3 YELLOW",
+    precio:1520000,
     estado: true,
-    foto:"https://firebasestorage.googleapis.com/v0/b/clinicadelplay-cf1d6.appspot.com/o/61Kda%2BeUmlL._SL1000_.jpg?alt=media&token=67359025-390d-46d9-bfe2-3f14ccca7a07    "
+    foto:"https://firebasestorage.googleapis.com/v0/b/tiendasufragio.appspot.com/o/SILLA%20amarilla.webp?alt=media&token=074d6812-9f3f-415d-8317-f93246bb45bc",
+    popularidad: "5/5",
+    pais: "USA"
 }
  
 let producto5={
-    nombre:"producto5",
-    precio:45000,
+    nombre:"COMBO CORE I3 + GTX 1650",
+    precio:3648000,
     estado: true,
-    foto:"https://firebasestorage.googleapis.com/v0/b/clinicadelplay-cf1d6.appspot.com/o/61Om-iwnAbL._AC_SY450_.jpg?alt=media&token=81c7f41c-a715-47c2-86ca-83a15db8c751"
+    foto:"https://firebasestorage.googleapis.com/v0/b/tiendasufragio.appspot.com/o/combo%201.webp?alt=media&token=8967948b-5684-44ed-8071-4ba749f1d85a",
+    popularidad: "3/5",
+    pais: "USA"
 }
  
 let producto6={
-    nombre:"producto6",
-    precio: 75000,
+    nombre:"AURORA COLORS RGB - VSG",
+    precio: 108000,
     estado: true,
-    foto:"https://firebasestorage.googleapis.com/v0/b/clinicadelplay-cf1d6.appspot.com/o/61zWIw7-S3S._SX342_.jpg?alt=media&token=c39c21fd-dd94-4a8c-9eea-440ae0575b1f"
+    foto:"https://firebasestorage.googleapis.com/v0/b/tiendasufragio.appspot.com/o/MOUSE%20AURORA.webp?alt=media&token=1b539260-7ab0-4220-b810-36eaf077f336",
+    popularidad: "4/5",
+    pais: "USA"
 }
  
 let producto7={
-    nombre:"producto7",
-    precio:75000,
+    nombre:"GEMINI BLACK / MULTIPLATAFORMA",
+    precio:133000,
     estado: true,
-    foto:"https://firebasestorage.googleapis.com/v0/b/clinicadelplay-cf1d6.appspot.com/o/81n2llGXyiL._SX342_.jpg?alt=media&token=6c60563a-9747-432b-b001-4166123315f4"
+    foto:"https://firebasestorage.googleapis.com/v0/b/tiendasufragio.appspot.com/o/AUDIFONOS%20GEMINI.webp?alt=media&token=257340a8-04f5-4c76-921f-8755ef68311c",
+    popularidad: "2/5",
+    pais: "USA"
 }
  
 let producto8={
-    nombre:"producto8",
-    precio:180000,
+    nombre:"KUMARA GRB MECÁNICO",
+    precio:228000,
     estado: true,
-    foto:"https://firebasestorage.googleapis.com/v0/b/clinicadelplay-cf1d6.appspot.com/o/9440503_2.jfif?alt=media&token=2c6b7aea-1be8-4be1-a4c9-49cb2d44ff56"
+    foto:"https://firebasestorage.googleapis.com/v0/b/tiendasufragio.appspot.com/o/TECLADO%20KUMARA.webp?alt=media&token=6cfcfc42-45b7-41cc-ada6-025e0c8d494a",
+    popularidad: "5/5",
+    pais: "USA"
 }
  
 let producto9={
-    nombre:"producto9",
-    precio:300000,
+    nombre:"VIPER MINI RGB - RAZER",
+    precio:171000,
     estado: true,
-    foto:"https://firebasestorage.googleapis.com/v0/b/clinicadelplay-cf1d6.appspot.com/o/Aud%C3%ADfonos-Sony-Ps4-Gold-Wireless-Heasdet-Ps4.jpg?alt=media&token=2d4a44ae-0f15-4c65-9c24-ee4f6fff08a5"
+    foto:"https://firebasestorage.googleapis.com/v0/b/tiendasufragio.appspot.com/o/TECLADO%20KUMARA.webp?alt=media&token=6cfcfc42-45b7-41cc-ada6-025e0c8d494a",
+    popularidad: "5/5",
+    pais: "USA"
 }
  
 let producto10={
-    nombre:"producto10",
-    precio:15000,
-    estado: true,
-    foto:"https://firebasestorage.googleapis.com/v0/b/clinicadelplay-cf1d6.appspot.com/o/D_NQ_NP_742938-MCO40052446796_122019-O.webp?alt=media&token=af451703-4530-4179-b673-28d011c603b5"
+    nombre:"PRIMER GAMING RGB - XPG",
+    precio:150000,
+    estado: false,
+    foto: "https://firebasestorage.googleapis.com/v0/b/tiendasufragio.appspot.com/o/MOUSE%20XPG.webp?alt=media&token=ed3f68f4-8e8b-4dc7-a2a4-fb7c96df41a9",
+    popularidad: "4/5",
+    pais: "COLOMBIA"
 }
  
 let producto11={
-    nombre:"producto11",
-    precio:15000,
+    nombre:"LOGITECH G203 / RGB",
+    precio:135000,
     estado: true,
-    foto:"https://firebasestorage.googleapis.com/v0/b/clinicadelplay-cf1d6.appspot.com/o/D_NQ_NP_797194-MCO43412176402_092020-O.jpg?alt=media&token=e3655d92-f730-41b9-a610-dce877ccc4a2"
+    foto:"https://firebasestorage.googleapis.com/v0/b/tiendasufragio.appspot.com/o/G203.png?alt=media&token=a9e4fe2c-2d4f-4b0a-980a-7f6acc4b692d",
+    popularidad: "5/5",
+    pais: "USA"
 }
  
 let producto12={
-    nombre:"producto12",
-    precio:190000,
-    estado: false,
-    foto:"https://firebasestorage.googleapis.com/v0/b/clinicadelplay-cf1d6.appspot.com/o/PS5-DualSense-MidnightBlack.jpg?alt=media&token=facdaf23-b00b-4ffa-a890-782f057025e7   "
+    nombre:"COMBO 4 EN 1 LEGION GAMER",
+    precio:228000,
+    estado: true,
+    foto:"https://firebasestorage.googleapis.com/v0/b/tiendasufragio.appspot.com/o/TDAGGER.png?alt=media&token=a14fb664-3d11-4480-9bc3-45138e867b51",
+    popularidad: "4/5",
+    pais: "USA"
 }
  
 let producto13={
-    nombre:"producto13",
-    precio:5000,
+    nombre:"SINGULARITY RGB WHITE/BLACK",
+    precio: 226000,
     estado: true,
-    foto:"https://firebasestorage.googleapis.com/v0/b/clinicadelplay-cf1d6.appspot.com/o/bdedc49a1ea3bc7f7a643b519c5bedf8-product.jpg?alt=media&token=9a0e98f9-91fe-4ad6-88e1-0faf367f0e00"
+    foto:"https://firebasestorage.googleapis.com/v0/b/tiendasufragio.appspot.com/o/SINGULARITY.png?alt=media&token=cc152cee-fb3a-4c4b-b322-9a60c348a1e8",
+    popularidad: "4/5",
+    pais: "USA"
 }
  
 let producto14={
     nombre:"producto14",
     precio:296000,
     estado: true,
-    foto:"https://firebasestorage.googleapis.com/v0/b/clinicadelplay-cf1d6.appspot.com/o/g502-lightspeed-hero.png?alt=media&token=dae69214-511f-490f-bff0-6c020ef3996a"
+    foto:"https://firebasestorage.googleapis.com/v0/b/clinicadelplay-cf1d6.appspot.com/o/g502-lightspeed-hero.png?alt=media&token=dae69214-511f-490f-bff0-6c020ef3996a",
+    popularidad: "",
+    pais: "USA"
 }
  
 let producto15={
     nombre:"producto15",
     precio:500000,
     estado: false,
-    foto:"https://firebasestorage.googleapis.com/v0/b/clinicadelplay-cf1d6.appspot.com/o/tv.jpg?alt=media&token=fe2924bb-d1ba-4bbf-b4f9-13846245fd33"
+    foto:"https://firebasestorage.googleapis.com/v0/b/clinicadelplay-cf1d6.appspot.com/o/tv.jpg?alt=media&token=fe2924bb-d1ba-4bbf-b4f9-13846245fd33",
+    popularidad: "",
+    pais: "USA"
 }
 let producto16={
     nombre:"producto16",
     precio:4300000,
     estado: true,
-    foto:"https://firebasestorage.googleapis.com/v0/b/clinicadelplay-cf1d6.appspot.com/o/iphone-11-pro-max.jpg?alt=media&token=3e98a0af-d086-495c-afb8-6b60a2734587"
+    foto:"https://firebasestorage.googleapis.com/v0/b/clinicadelplay-cf1d6.appspot.com/o/iphone-11-pro-max.jpg?alt=media&token=3e98a0af-d086-495c-afb8-6b60a2734587",
+    popularidad: "",
+    pais: "USA"
 }
 let producto17={
     nombre:"producto17",
     precio:5600000,
     estado: true,
-    foto:"https://firebasestorage.googleapis.com/v0/b/clinicadelplay-cf1d6.appspot.com/o/iphone-12-pro-max.jfif?alt=media&token=209457d4-6051-4b94-a84a-8cd459d633c4"
+    foto:"https://firebasestorage.googleapis.com/v0/b/clinicadelplay-cf1d6.appspot.com/o/iphone-12-pro-max.jfif?alt=media&token=209457d4-6051-4b94-a84a-8cd459d633c4",
+    popularidad: "",
+    pais: "USA"
 }
 let producto18={
     nombre:"producto18",
     precio:5200000,
     estado: true,
-    foto:"https://firebasestorage.googleapis.com/v0/b/clinicadelplay-cf1d6.appspot.com/o/iphone-13-mini-blue.jfif?alt=media&token=cf4879f8-8ebc-4d3f-9b83-8efc64718273"
+    foto:"https://firebasestorage.googleapis.com/v0/b/clinicadelplay-cf1d6.appspot.com/o/iphone-13-mini-blue.jfif?alt=media&token=cf4879f8-8ebc-4d3f-9b83-8efc64718273",
+    popularidad: "",
+    pais: "USA"
 }
 let producto19={
     nombre:"producto19",
     precio:6300000,
     estado: true,
-    foto:"https://firebasestorage.googleapis.com/v0/b/clinicadelplay-cf1d6.appspot.com/o/iphone13pro.jpg?alt=media&token=14f77760-46d7-49a7-83e4-fa749715816e"
+    foto:"https://firebasestorage.googleapis.com/v0/b/clinicadelplay-cf1d6.appspot.com/o/iphone13pro.jpg?alt=media&token=14f77760-46d7-49a7-83e4-fa749715816e",
+    popularidad: "",
+    pais: "USA"
 }
 let producto20={
     nombre:"producto20",
     precio:3900000,
     estado: true,
-    foto:"https://firebasestorage.googleapis.com/v0/b/clinicadelplay-cf1d6.appspot.com/o/iphone13.jfif?alt=media&token=b25e7cf5-413b-42fc-b7b8-7841b1d0c822"
+    foto:"https://firebasestorage.googleapis.com/v0/b/clinicadelplay-cf1d6.appspot.com/o/iphone13.jfif?alt=media&token=b25e7cf5-413b-42fc-b7b8-7841b1d0c822",
+    popularidad: "",
+    pais: "USA"
 }
  
 //ARREGLO
@@ -167,7 +207,23 @@ productos.forEach(function(producto){
     columna.classList.add("col")
     let tarjeta = document.createElement("div")
     tarjeta.classList.add("card")
+    let nombreProducto = document.createElement("h5")
+    nombreProducto.classList.add("text-center")
+    nombreProducto.textContent = producto.nombre
+    let precio = document.createElement("h5") 
+    precio.classList.add("text-center")
+    precio.textContent = "$" + producto.precio
+    let popularidad = document.createElement("h5")
+    popularidad.classList.add("text-center")
+    popularidad.textContent = producto.popularidad
+    let pais = document.createElement("h5")
+    pais.classList.add("text-center")
+    pais.textContent = producto.pais
     tarjeta.appendChild(foto)
+    tarjeta.appendChild(nombreProducto)
+    tarjeta.appendChild(precio)
+    tarjeta.appendChild(popularidad)
+    tarjeta.appendChild(pais)
     columna.appendChild(tarjeta)
     contenedor.appendChild(columna)
     
